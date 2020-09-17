14011b3ea5bfeb232be5055319a31d80f88d2482
mudi <935522618@qq.com>
2020-09-17 13:48:11 +0800

    new edit

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -1,10 +1,7 @@
 import argparse
-
 import json
 import os
-
-
-class Data:
+class InputData:
 
     def __init__(self, path: str = None):
         self.__dir_addr = path
@@ -13,13 +10,13 @@ class Data:
             print("初次运行初始化")
             if not os.path.exists(self.__dir_addr):
                 raise RuntimeError("Path doesn't exist.")
-            self.__read_1()
-            self.__analysis()
+            self.Init()
+            self.Analysis()
             self.__save2json()
         else:
-            self.__read_2()
+            self.sovle()
 
-    def __read_1(self):
+    def Init(self):
         self.__dicts = []
         for root, dirs, files in os.walk(self.__dir_addr):
             for file in files:
@@ -29,11 +26,11 @@ class Data:
                         for self.__json in self.__jsons:
                             self.__dicts.append(json.loads(self.__json))
 
-    def __analysis(self):
+    def Analysis(self):
         self.__types = ['PushEvent', 'IssueCommentEvent', 'IssuesEvent', 'PullRequestEvent']
-        self.__cnt_perP = {}
-        self.__cnt_perR = {}
-        self.__cnt_perPperR = {}
+        self.CountOfPerP = {}
+        self.CountOfPerR = {}
+        self.CountOfPerPperR = {}
 
         for self.__dict in self.__dicts:
             # 如果属于四种事件之一 则增加相应值
@@ -41,41 +38,41 @@ class Data:
                 self.__event = self.__dict['type']
                 self.__name = self.__dict['actor']['login']
                 self.__repo = self.__dict['repo']['name']
-                self.__cnt_perP[self.__name + self.__event] = self.__cnt_perP.get(self.__name + self.__event, 0) + 1
-                self.__cnt_perR[self.__repo + self.__event] = self.__cnt_perP.get(self.__repo + self.__event, 0) + 1
-                self.__cnt_perPperR[self.__name + self.__repo + self.__event] = self.__cnt_perPperR.get(
+                self.CountOfPerP[self.__name + self.__event] = self.CountOfPerP.get(self.__name + self.__event, 0) + 1
+                self.CountOfPerR[self.__repo + self.__event] = self.CountOfPerR.get(self.__repo + self.__event, 0) + 1
+                self.CountOfPerPperR[self.__name + self.__repo + self.__event] = self.CountOfPerPperR.get(
                     self.__name + self.__repo + self.__event, 0) + 1
 
     def __save2json(self):
         with open("1.json", 'w', encoding='utf-8') as f:
-            json.dump(self.__cnt_perP, f)
+            json.dump(self.CountOfPerP, f)
         with open("2.json", 'w', encoding='utf-8') as f:
-            json.dump(self.__cnt_perR, f)
+            json.dump(self.CountOfPerR, f)
         with open("3.json", 'w', encoding='utf-8') as f:
-            json.dump(self.__cnt_perPperR, f)
+            json.dump(self.CountOfPerPperR, f)
         print("Save to json files successfully!")
 
-    def __read_2(self):
-        self.__cnt_perP = {}
-        self.__cnt_perR = {}
-        self.__cnt_perPperR = {}
+    def sovle(self):
+        self.CountOfPerP = {}
+        self.CountOfPerR = {}
+        self.CountOfPerPperR = {}
         with open("1.json", encoding='utf-8') as f:
-            self.__cnt_perP = json.load(f)
+            self.CountOfPerP = json.load(f)
         with open("2.json", encoding='utf-8') as f:
-            self.__cnt_perR = json.load(f)
+            self.CountOfPerR = json.load(f)
         with open("1.json", encoding='utf-8') as f:
-            self.__cnt_perPperR = json.load(f)
+            self.CountOfPerPperR = json.load(f)
 
     # get value from dictionary
 
     def get_cnt_user(self, user: str, event: str) -> int:
-        return self.__cnt_perP.get(user + event, 0)
+        return self.CountOfPerP.get(user + event, 0)
 
     def get_cnt_repo(self, repo: str, event: str) -> int:
-        return self.__cnt_perR.get(repo + event, 0)
+        return self.CountOfPerR.get(repo + event, 0)
 
     def get_cnt_user_and_repo(self, user, repo, event) -> int:
-        return self.__cnt_perPperR.get(user + repo + event, 0)
+        return self.CountOfPerPperR.get(user + repo + event, 0)
 def run():
     my_parser = argparse.ArgumentParser(description='analysis the json file')
     my_parser.add_argument('-i', '--init', help='json file path')
@@ -85,18 +82,18 @@ def run():
     args = my_parser.parse_args()
 
     if args.init:
-        my_data = Data(path=args.init)
+        my_InputData = InputData(path=args.init)
     else:
-        my_data = Data()
+        my_InputData = InputData()
         if args.event:
             if args.user:
                 if args.repo:
-                    print(my_data.get_cnt_user_and_repo(args.user, args.repo, args.event))
+                    print(my_InputData.get_cnt_user_and_repo(args.user, args.repo, args.event))
                 else:
-                    print(my_data.get_cnt_user(args.user, args.event))
+                    print(my_InputData.get_cnt_user(args.user, args.event))
             else:
                 if args.repo:
-                    print(my_data.get_cnt_repo(args.repo, args.event))
+                    print(my_InputData.get_cnt_repo(args.repo, args.event))
                 else:
                     print("missing argument: user or repo")
         else:
